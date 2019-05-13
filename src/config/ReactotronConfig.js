5168f676a49817038e7afb45b8a1ad672a33de9f
Giovani Mantovani <giovani.mantovani@agrotools.com.br>
2019-05-13 09:49:41 -0300

    ESLINT and Reactotron config

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure()
+  .useReactNative()
+  .connect();
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import './config/ReactotronConfig';
+
+const App = () => (
+  <View>
+    <Text>Welcome</Text>
+  </View>
+);
+
+export default App;
